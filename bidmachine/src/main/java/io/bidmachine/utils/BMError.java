4d088bfd0bdb89bfe8beb01cbf4a8beeb886d8a0
nikita_cherep <DemonD600@mail.ru>
2021-07-19 13:10:37 +0300

    Feature/APDX-3377: Added error codes to BMError

--- bidmachine/src/main/java/io/bidmachine/utils/BMError.java
+++ bidmachine/src/main/java/io/bidmachine/utils/BMError.java
@@ -7,22 +7,32 @@ import io.bidmachine.protobuf.ErrorReason;
 public class BMError {
 
     public static final int NOT_SET = -1;
+    public static final int NO_CONNECTION = ErrorReason.ERROR_REASON_NO_CONNECTION_VALUE;
+    public static final int TIMEOUT = ErrorReason.ERROR_REASON_TIMEOUT_VALUE;
+    public static final int HTTP_BAD_REQUEST = ErrorReason.ERROR_REASON_HTTP_BAD_REQUEST_VALUE;
+    public static final int SERVER = ErrorReason.ERROR_REASON_HTTP_SERVER_ERROR_VALUE;
+    public static final int NO_CONTENT = ErrorReason.ERROR_REASON_NO_CONTENT_VALUE;
+    public static final int BAD_CONTENT = ErrorReason.ERROR_REASON_BAD_CONTENT_VALUE;
+    public static final int HB_NETWORK = ErrorReason.ERROR_REASON_HB_NETWORK_VALUE;
+    public static final int EXPIRED = ErrorReason.ERROR_REASON_WAS_EXPIRED_VALUE;
+    public static final int DESTROYED = ErrorReason.ERROR_REASON_WAS_DESTROYED_VALUE;
+    public static final int INTERNAL = ErrorReason.ERROR_REASON_INTERNAL_VALUE;
 
 
     public static final BMError NoConnection =
-            new BMError(ErrorReason.ERROR_REASON_NO_CONNECTION_VALUE,
+            new BMError(NO_CONNECTION,
                         "Can't connect to server");
 
     public static final BMError TimeoutError =
-            new BMError(ErrorReason.ERROR_REASON_TIMEOUT_VALUE,
+            new BMError(TIMEOUT,
                         "Timeout reached");
 
     public static final BMError Request =
-            new BMError(ErrorReason.ERROR_REASON_HTTP_BAD_REQUEST_VALUE,
+            new BMError(HTTP_BAD_REQUEST,
                         "Request contains bad syntax or cannot be fulfilled");
 
     public static final BMError Server =
-            new BMError(ErrorReason.ERROR_REASON_HTTP_SERVER_ERROR_VALUE,
+            new BMError(SERVER,
                         "Server failed to fulfil an apparently valid request");
 
     public static final BMError RequestAlreadyShown =
@@ -30,11 +40,11 @@ public class BMError {
                         "AdRequest that related with ad has already been shown, load new AdRequest please");
 
     public static final BMError RequestExpired =
-            new BMError(ErrorReason.ERROR_REASON_WAS_EXPIRED_VALUE,
+            new BMError(EXPIRED,
                         "AdRequest expired, load new one please");
 
     public static final BMError RequestDestroyed =
-            new BMError(ErrorReason.ERROR_REASON_WAS_DESTROYED_VALUE,
+            new BMError(DESTROYED,
                         "AdRequest destroyed, create new one please");
 
     public static final BMError AlreadyShown =
@@ -42,30 +52,30 @@ public class BMError {
                         "Ads was already shown, load new one please");
 
     public static final BMError Expired =
-            new BMError(ErrorReason.ERROR_REASON_WAS_EXPIRED_VALUE,
+            new BMError(EXPIRED,
                         "Ads was expired, load new one please");
 
     public static final BMError Destroyed =
-            new BMError(ErrorReason.ERROR_REASON_WAS_DESTROYED_VALUE,
+            new BMError(DESTROYED,
                         "Ads destroyed, load new one please");
 
 
     public static BMError noFill() {
-        return new BMError(ErrorReason.ERROR_REASON_NO_CONTENT_VALUE, "No ads fill");
+        return new BMError(NO_CONTENT, "No ads fill");
     }
 
     public static BMError notFound(@NonNull String name) {
-        return new BMError(ErrorReason.ERROR_REASON_NO_CONTENT_VALUE,
+        return new BMError(NO_CONTENT,
                            String.format("%s not found", name));
     }
 
     public static BMError incorrectContent(@NonNull String message) {
-        return new BMError(ErrorReason.ERROR_REASON_BAD_CONTENT_VALUE, message);
+        return new BMError(BAD_CONTENT, message);
     }
 
 
     public static BMError adapter(@NonNull String message) {
-        return new BMError(ErrorReason.ERROR_REASON_HB_NETWORK_VALUE, message);
+        return new BMError(HB_NETWORK, message);
     }
 
     public static BMError adapterNotInitialized() {
@@ -82,7 +92,7 @@ public class BMError {
 
 
     public static BMError internal(@NonNull String message) {
-        return new BMError(ErrorReason.ERROR_REASON_INTERNAL_VALUE, message);
+        return new BMError(INTERNAL, message);
     }
 
 
